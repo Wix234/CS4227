a6064ac3a587aa6ef7cc691690d1b147276b2d7a
Wix234 <61157446+Wix234@users.noreply.github.com>
2023-02-24 13:40:14 +0000

    Added User

--- AuthorisationInterceptor.java
+++ AuthorisationInterceptor.java
@@ -1,5 +1,14 @@
+//concrete interceptor
 public class AuthorisationInterceptor implements Interceptor {
 	public void execute(String request) {
 		System.out.println("In authorisation interceptor" + " " + request);
 	}
-    }
+
+	public void execute2(User user) {
+		if (user.getID() == 12345 && user.getPass() == "Pandas"){
+			System.out.println("User has been authorized");
+		}else{
+			System.out.println("User details are not in the system");
+		}
+	}
+}
