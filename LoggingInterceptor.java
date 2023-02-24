7ae82e45c1a23e9f810f74f77fa6db9292cf26a4
Wix234 <61157446+Wix234@users.noreply.github.com>
2023-02-24 13:41:39 +0000

    Added User

--- LoggingInterceptor.java
+++ LoggingInterceptor.java
@@ -2,4 +2,12 @@ public class LoggingInterceptor implements Interceptor {
 	public void execute(String request) {
 		System.out.println("In logging interceptor" + " " + request);
 	}
+
+	public void execute2(User user) {
+		if (user.getID() == 12345 && user.getPass() == "Pandas"){
+			System.out.println("User has been logged in");
+		}else{
+			System.out.println("User has not been logged in");
+		}
+	}
 }
