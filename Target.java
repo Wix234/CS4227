b48d054329cb5233dc5c081ea1e8dcf7f149ae1f
Wix234 <61157446+Wix234@users.noreply.github.com>
2023-02-24 13:42:40 +0000

    Added User

--- Target.java
+++ Target.java
@@ -2,4 +2,8 @@ public class Target {
     public void execute(String request){
        System.out.println("Executing request: " + " " + request);
     }
+
+    public void execute2(User user){
+      System.out.println("User logged in successfully:" + " " + user.getID());
+   }
  }
