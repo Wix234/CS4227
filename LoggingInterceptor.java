972b44204db7c5aa9aaa94f9e780e3e7ec1ab4cc
Wix234 <61157446+Wix234@users.noreply.github.com>
2023-02-26 04:16:50 +0000

    Update LoggingInterceptor.java

--- LoggingInterceptor.java
+++ LoggingInterceptor.java
@@ -1,13 +1,34 @@
+//concrete interceptor
+
 public class LoggingInterceptor implements Interceptor {
+
+	private String[][] users = {{"12345", "Pandas"}, {"67890","Blob"}};
+
 	public void execute(String request) {
 		System.out.println("In logging interceptor" + " " + request);
 	}
 
-	public void execute2(User user) {
-		if (user.getID() == 12345 && user.getPass() == "Pandas"){
-			System.out.println("User has been logged in");
-		}else{
-			System.out.println("User has not been logged in");
+	public boolean execute2(User user) {
+		boolean userExists = false; 
+		for(int i = 0; i < users.length; i++){
+			for(int j = 0; j < users[i].length; j++){
+				if(users[i][j].equals(Integer.toString(user.getID()))){
+					if(users[i][j+1].equals(user.getPass())){
+						userExists = true;
+						System.out.println("User " + user.getID() + " has been logged in");
+						return true;
+					}else{
+						userExists = true;
+						System.out.println("User " + user.getID() + " has not been logged in");
+						return false;
+					}
+				}  
+			}  
 		}
+
+		if (userExists == false){
+			System.out.println("Login attempt failed");
+		}
+		return false;
 	}
 }
